c109c045ae0e215b21df4a3b13f82162aa2e8f4a
juviblakk <92520371+juviblakk@users.noreply.github.com>
2021-11-09 22:54:44 -0500

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,17 +1,17 @@
 <?php
     //Development Connection
-    $host='127.0.0.1';
+    /*$host='127.0.0.1';
     $db='attendance';
     $user='root';
     $pass='';
-    $charset='utf8mb4';
+    $charset='utf8mb4';*/
 
     //Remote Database Connection
-    /*$host='remotemysql.com';
+    $host='remotemysql.com';
     $db='sZkIzyrnvg';
     $user='sZkIzyrnvg';
     $pass='Ta3Iiilhl6';
-    $charset='utf8mb4';*/
+    $charset='utf8mb4';
 
     $dsn= "mysql:host=$host;dbname=$db;charset=$charset";
 
